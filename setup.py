e91a5595c195147d1f290d96cbe4c3f268f3d4df
Alishaw99 <tariqham@gmail.com>
2024-05-21 06:21:02 +0500

    utils-added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="ner-using-bert",
+    version="0.0.1",
+    author="Ali Syed",
+    author_email="tariqham@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
