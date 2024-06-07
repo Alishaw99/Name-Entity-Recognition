9892f2f79dccc31537054334bbb2c05648a3f0d3
Alishaw99 <tariqham@gmail.com>
2024-06-07 10:16:12 -0700

    data ingest_comp

--- ner/entity/artifact_entity.py
+++ ner/entity/artifact_entity.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 
-
 # Data Ingestion Artifacts
+
 @dataclass
 class DataIngestionArtifacts:
     zip_data_file_path: str
@@ -9,15 +9,15 @@ class DataIngestionArtifacts:
 
 
 
-# Data Transformation Artifacts
-@dataclass
-class DataTransformationArtifacts:
-    labels_to_ids_path: str
-    ids_to_labels_path: str
-    df_train_path: str
-    df_val_path: str
-    df_test_path: str
-    unique_labels_path: str
+# # Data Transformation Artifacts
+# @dataclass
+# class DataTransformationArtifacts:
+#     labels_to_ids_path: str
+#     ids_to_labels_path: str
+#     df_train_path: str
+#     df_val_path: str
+#     df_test_path: str
+#     unique_labels_path: str
 
 
 
